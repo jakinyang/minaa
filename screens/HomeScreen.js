ecddf46d18b3a10521ef31628e51cb69beb1ab28
Benjamin Meng <benjaminmeng1994@gmail.com>
2023-01-07 17:53:44 -0800

    --add mockmarker data and markers

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -19,6 +19,66 @@ export default function HomeScreen({ navigation }) {
     longitude: -122.4324,
   };
 
+  const mockReportData = [
+    {
+      id: 0,
+      coords:{
+        latitude: 37.7130,
+        longitude: -122.4102
+      },
+      img: ""
+    },
+    {
+      id: 1,
+      coords: {
+        latitude: 37.7684,
+        longitude: -122.4102
+      },
+      img:""
+    },
+    {
+      id: 2,
+      coords: {
+        latitude: 37.7345,
+        longitude: -122.5128
+      },
+      img:""
+    },
+  ]
+
+  function MapMarkers() {
+    return (
+      mockReportData.map((item, i) => {
+        return (
+          <Marker
+          coordinate={item.coords}
+          key={item.id}
+          > 
+          </Marker>
+        )
+      })
+      // <Marker
+      // draggable
+      // coordinate={initialMarkerRegion}
+      // onDragEnd={
+      //   (e) => setMarkerRegion({...markerRegion, latitude: e.nativeEvent.coordinate.latitude, longitude: e.nativeEvent.coordinate.longitude})}
+
+      // >
+      // </Marker>
+    )
+  }
+
+
+  const [region, setRegion] = React.useState(initialRegion);
+  const [markerRegion, setMarkerRegion] = React.useState(initialMarkerRegion);
+  const mapRef = React.useRef(null);
+
+  const resetRegionHandler = () => {
+    mapRef.current.animateToRegion(initialRegion, 1 * 1000);
+  };
+
+  
+
   return (
     <View style={styles.container}>
     <Text>Home Screen</Text>
@@ -30,10 +90,28 @@ export default function HomeScreen({ navigation }) {
   title="Go to Profile Tab"
   onPress={() => navigation.navigate('ProfileTab')}
 />
+
+<Button title='reset location' onPress={resetRegionHandler}/>
+
+<Text>Current lat and lon:</Text>
+ <Text>{region.latitude}, {region.longitude}</Text>
     <MapView 
     style={styles.map}
     initialRegion={initialRegion}
+    provider={PROVIDER_GOOGLE}
+    onRegionChangeComplete={(region) => setRegion(region)}
+    ref={mapRef}
     >
+      <Marker
+      draggable
+      coordinate={initialMarkerRegion}
+      onDragEnd={
+        (e) => setMarkerRegion({...markerRegion, latitude: e.nativeEvent.coordinate.latitude, longitude: e.nativeEvent.coordinate.longitude})}
+
+      >
+      </Marker>
+
+      <MapMarkers />
 
     </MapView>
     
