426380988477299206ed8337923c1c89631ed8c9
bohjaangles <bohjaangles@gmail.com>
2023-01-10 01:03:33 -0600

    add the core functionality of hte report form to the newReportScreen

--- screens/NewReportScreen.js
+++ screens/NewReportScreen.js
@@ -1,13 +1,61 @@
 import * as React from 'react';
-import { View, Button,StyleSheet } from 'react-native';
-import { Dialog, Portal, Text } from 'react-native-paper';
+import { useState } from 'react';
+import { View, Button,StyleSheet, ScrollView } from 'react-native';
+import { Dialog, Portal, Text, TextInput } from 'react-native-paper';
+import DropDownPicker from 'react-native-dropdown-picker';
+
 
 export default function NewReportScreen() {
+  const [open, setOpen] = useState(false);
+  const [description, setDescription] = useState("")
+  const [reportSizeValue, setReportSizeValue] = useState(5)
+  const [reportSizeItem, setReportSizeItem] = useState([
+    { label: "5 Meters", value: 5 },
+    { label: "10 Meters", value: 10 },
+    { label: "15 Meters", value: 15 },
+    { label: "30 Meters", value: 30 },
+    { label: "50 Meters", value: 50 },
+  ])
+  const [reportValue, setReportValue] = useState(null)
+  const [reportItems, setReportItems] = useState([
+    { label: "Unclear", value: "unclear"},
+    { label: "Obscured", value: "obscured"},
+    { label: "Multiple", value: "multiple"},
+    { label: "Large", value: "large"},
+    { label: "Small", value: "small"},
+  ])
 
   return (
-    <View>
-      <Text>This is a new report </Text>
-
-    </View>
+    <ScrollView style={{ flex: 1, width: "100%", height: "100%"}}>
+      <View>
+        <Text> New Report </Text>
+        <TextInput 
+          mode='outlined'
+          label="description"
+          placeholder='what do you see?'
+          onChangeText={(text) => setDescription({value: text})}
+          multiline="true"
+          returnKeyType='next'
+        />
+        <Text> Report type </Text>
+        <DropDownPicker 
+          open={open}
+          value={reportValue}
+          items={reportItems}
+          setOpen={setOpen}
+          setItems={setReportItems}
+          setValue={setReportValue}
+        />
+        <Text> Area of concern in meters </Text>
+        <DropDownPicker 
+          open={open}
+          value={reportSizeValue}
+          items={reportSizeItem}
+          setOpen={setOpen}
+          setItems={setReportSizeItem}
+          setValue={setReportSizeValue}
+        />
+      </View>
+    </ScrollView>
   )
 }
