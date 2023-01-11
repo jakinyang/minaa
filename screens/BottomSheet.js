b85c23e5d2ea99f3c99911be0d5535f5c7fd9b9b
jakinyang <109392405+jakinyang@users.noreply.github.com>
2023-01-11 09:24:31 -0800

    --update Bottomsheet

--- screens/BottomSheet.js
+++ screens/BottomSheet.js
@@ -1,18 +1,18 @@
-import React, {useRef, useMemo} from 'react';
+import React, { useRef, useMemo } from 'react';
 import { StyleSheet } from 'react-native';
 import { BottomSheetModal } from '@gorhom/bottom-sheet';
 import ResourceIndex from './Resources/ResourceIndexScreen';
 
-export default function BottomSheet({navigation, bottomSheetModalRef, snapPoints}) {
-  return(
+export default function BottomSheet({ navigation, bottomSheetModalRef, snapPoints, children }) {
+  return (
     <BottomSheetModal
-        ref={bottomSheetModalRef}
-        index={0}
-        snapPoints={snapPoints}
-        style={styles.bottomSheet}
-      >
-        <ResourceIndex />
-      </BottomSheetModal>
+      ref={bottomSheetModalRef}
+      index={0}
+      snapPoints={snapPoints}
+      style={styles.bottomSheet}
+    >
+      {children}
+    </BottomSheetModal>
   )
 }
 
