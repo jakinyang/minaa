46ac48541ab5be2fb80503ba86ee02fe23c53aa2
jakinyang <109392405+jakinyang@users.noreply.github.com>
2023-01-11 19:35:16 -0800

    --add sample.js

---
+++ src/sample.js
@@ -0,0 +1,67 @@
+import * as React from "react";
+import { View, Button, Text, StyleSheet, FlatList } from "react-native";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  gql,
+  useQuery,
+} from "@apollo/client";
+
+function TestUser({ id, firstName, lastName, email }) {
+  return (
+    <View>
+      <Text style={styles.item1}>
+        "id": {id} 
+      </Text>
+      <Text style={styles.item2}>
+      "first name" {firstName}
+      </Text>
+      <Text style={styles.item3}>
+      "last name" {lastName}
+      </Text>
+      <Text style={styles.item4}>
+         "email: {email}
+      </Text>
+    </View>
+  );
+}
+
+export default function Test() {
+  const GET_USER_TEST = gql`
+    query Query {
+      users {
+        id
+        firstName
+        lastName
+        email
+      }
+    }
+  `;
+
+  const { loading, error, data } = useQuery(GET_USER_TEST);
+
+  if (loading) {
+    return <Text>Loading...</Text>;
+  }
+  if (error) {
+    console.log("Error from useQuery Test!");
+  }
+  // console.log(data.users);
+  return (
+    <View>
+      <FlatList
+        data={data.users}
+        renderItem={({ item }) => (
+          <TestUser
+            id={item.id}
+            firstName={item.firstName}
+            lastName={item.lastName}
+            email={item.email}
+          />
+        )}
+        keyExtractor={(item) => item.id}
+      />
+    </View>
+  );
+}
