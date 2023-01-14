58d6d5435c721344074f51d889923d4c0b0d02da
jakinyang <109392405+jakinyang@users.noreply.github.com>
2023-01-13 19:44:59 -0800

    --add login features and context

--- App.js
+++ App.js
@@ -1,15 +1,17 @@
+import React from 'react';
 import 'react-native-gesture-handler';
 import { StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { Provider as PaperProvider } from 'react-native-paper';
 import HomeStack from './routes/HomeStack';
-import { 
-  ApolloClient, 
-  InMemoryCache, 
-  ApolloProvider, 
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
   HttpLink
 } from '@apollo/client';
 import { BottomSheetModalProvider } from '@gorhom/bottom-sheet';
+import UserProvider from './shared/userContext';
 
 const link = new HttpLink({
   uri: 'http://localhost:4000/',
@@ -27,7 +29,9 @@ export default function App() {
       <ApolloProvider client={client}>
         <NavigationContainer>
           <BottomSheetModalProvider>
-            <HomeStack style={styles.container} />
+            <UserProvider>
+              <HomeStack style={styles.container} />
+            </UserProvider>
           </BottomSheetModalProvider>
         </NavigationContainer>
       </ApolloProvider>
--- screens/Login/LoginScreen.js
+++ screens/Login/LoginScreen.js
@@ -1,37 +1,64 @@
 // RN imports
-import React, { useState } from 'react'
+import React, { useEffect, useState, useContext } from 'react'
 import { TouchableOpacity, StyleSheet, View, SafeAreaView } from 'react-native'
 import { Text } from 'react-native-paper'
 
-// Component and theme imports
+//backend imports
+import { useLazyQuery } from '@apollo/client';
+import { CURRENT_USER } from '../../src/Queries/GetCurrentUser.js'
+
+// Component, route and theme imports
 import Background from './LoginComponents/Background.js'
 import Logo from './LoginComponents/Logo.js'
 import Header from './LoginComponents/Header.js'
 import Button from './LoginComponents/Button.js'
 import TextInput from './LoginComponents/TextInput.js'
 import BackButton from './LoginComponents/BackButton.js'
-// import { theme } from '../core/theme'
+import HomeStack from '../../routes/HomeStack.js'
 
 // Helper function imports
 import { emailValidator } from './LoginHelpers/emailValidator.js'
 import { passwordValidator } from './LoginHelpers/passwordValidator.js'
+import { UserContext } from '../../shared/userContext.js';
 
 export default function LoginScreen({ navigation }) {
   const [email, setEmail] = useState({ value: '', error: '' })
   const [password, setPassword] = useState({ value: '', error: '' })
+  const { user, setUser} = useContext(UserContext)
+
+    // "Janessa.Witting@gmail.com" "2bRUK1aPe0vj2wf"
+  const[getCurrentUser, { data, loading, error }] = useLazyQuery(CURRENT_USER, {
+    variables: {"search": {
+      "email": email.value
+    }},
+    fetchPolicy: 'cache-and-network',
+    onCompleted: (data) => {
+      console.log('oncompleted triggered');
+      if (data.usersSearch[0].email !== email.value || data.usersSearch[0].password !== password.value) {
+        alert("Invalid email or password, please try again")
+        return
+      }
+  
+      alert("Login successful")
+      setUser(data.usersSearch[0].id)
+
+      navigation.navigate('Home', {
+        screen: 'HomeScreen',
+      })
+    }
+  })
+  if (error) console.log(`Error ${error.message}`);
 
   const onLoginPressed = () => {
     const emailError = emailValidator(email.value)
     const passwordError = passwordValidator(password.value)
+    
     if (emailError || passwordError) {
       setEmail({ ...email, error: emailError })
       setPassword({ ...password, error: passwordError })
       return
     }
-    navigation.reset({
-      index: 0,
-      routes: [{ name: 'HomeStack' }],
-    })
+    getCurrentUser()
   }
 
   return (
@@ -102,91 +129,3 @@ const styles = StyleSheet.create({
     // color: theme.colors.primary,
   },
 })
-
-/*
-
-Something Basic I found to fall back to:
-
-import { StatusBar } from "expo-status-bar";
-import React, { useState } from "react";
-import {
-  StyleSheet,
-  Text,
-  View,
-  Image,
-  TextInput,
-  Button,
-  TouchableOpacity,
-} from "react-native";
-export default function App() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  return (
-    <View style={styles.container}>
-      <Image style={styles.image} source={require("./assets/log2.png")} /> 
-      <StatusBar style="auto" />
-      <View style={styles.inputView}>
-        <TextInput
-          style={styles.TextInput}
-          placeholder="Email."
-          placeholderTextColor="#003f5c"
-          onChangeText={(email) => setEmail(email)}
-        /> 
-      </View> 
-      <View style={styles.inputView}>
-        <TextInput
-          style={styles.TextInput}
-          placeholder="Password."
-          placeholderTextColor="#003f5c"
-          secureTextEntry={true}
-          onChangeText={(password) => setPassword(password)}
-        /> 
-      </View> 
-      <TouchableOpacity>
-        <Text style={styles.forgot_button}>Forgot Password?</Text> 
-      </TouchableOpacity> 
-      <TouchableOpacity style={styles.loginBtn}>
-        <Text style={styles.loginText}>LOGIN</Text> 
-      </TouchableOpacity> 
-    </View> 
-  );
-}
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  image: {
-    marginBottom: 40,
-  },
-  inputView: {
-    backgroundColor: "#FFC0CB",
-    borderRadius: 30,
-    width: "70%",
-    height: 45,
-    marginBottom: 20,
-    alignItems: "center",
-  },
-  TextInput: {
-    height: 50,
-    flex: 1,
-    padding: 10,
-    marginLeft: 20,
-  },
-  forgot_button: {
-    height: 30,
-    marginBottom: 30,
-  },
-  loginBtn: {
-    width: "80%",
-    borderRadius: 25,
-    height: 50,
-    alignItems: "center",
-    justifyContent: "center",
-    marginTop: 40,
-    backgroundColor: "#FF1493",
-  },
-});
-*/
